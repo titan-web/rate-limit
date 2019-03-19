ade73ccac811a393679bcdfedfbdce9019eb8f5e
wteo <woootao@gmail.com>
2019-03-19 18:17:24 +0800

    fix leaky_bucket.py

--- leaky_bucket/__init__.py
+++ leaky_bucket/__init__.py
@@ -1,34 +1,47 @@
 """An implementation of the leaky bucket algorithm.
 """
 from time import time
+from threading import RLock
 
 __all__ = ("LeakyBucket", )
 
 
 class LeakyBucket(object):
 
-    def __init__(self, capacity, fill_rate, is_lock=False):
+    def __init__(self, capacity, leak_rate, is_lock=False):
         """
         :param capacity:  The total tokens in the bucket.
-        :param fill_rate:  The rate in tokens/second that the bucket will be refilled
+        :param leak_rate:  The rate in tokens/second that the bucket leaks
         """
         self._capacity = float(capacity)
-        self._cur_tokens = 0
-        self._fill_rate = float(fill_rate)
-        self._is_lock = is_lock
+        self._used_tokens = 0
+        self._leak_rate = float(leak_rate)
         self._last_time = time()
+        self._lock = RLock() if is_lock else None
 
-    def get_cur_tokens(self):
+    def get_used_tokens(self):
+        if self._lock:
+            with self._lock:
+                return self._get_used_tokens()
+        else:
+            return self._get_used_tokens()
+
+    def _get_used_tokens(self):
         now = time()
-        delta = self._fill_rate * (now - self._last_time)
-        self._cur_tokens = max(0, self._cur_tokens + delta)
-        return self._cur_tokens
+        delta = self._leak_rate * (now - self._last_time)
+        self._used_tokens = max(0, self._used_tokens - delta)
+        return self._used_tokens
 
     def _consume(self, tokens):
-        if tokens <= self.get_cur_tokens():
-            self._cur_tokens += tokens
+        if tokens + self._get_used_tokens() <= self._capacity:
+            self._used_tokens += tokens
+            self._last_time = time()
             return True
         return False
 
     def consume(self, tokens):
-        return self._consume(tokens)
+        if self._lock:
+            with self._lock:
+                return self._consume(tokens)
+        else:
+            return self._consume(tokens)
